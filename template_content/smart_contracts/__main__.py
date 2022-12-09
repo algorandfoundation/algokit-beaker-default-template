c272d1468e8d526ea5e7ac35aed6edc068f5bbd2
Adam Chidlow <achidlow@users.noreply.github.com>
2022-12-09 16:29:09 +0800

    - make lab distillation take an output location rather than assume one - use qualname in case of nested classes

--- template_content/smart_contracts/__main__.py
+++ template_content/smart_contracts/__main__.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+
 from smart_contracts.lab import my_lab
 
-my_lab.distill()
+my_lab.distill(
+    output_dir=(Path(__file__).parent / "artifacts"),
+)
--- template_content/smart_contracts/lab.py
+++ template_content/smart_contracts/lab.py
@@ -10,16 +10,15 @@ class Lab:
     def __init__(self, apps: Sequence[Application]) -> None:
         self.apps = apps
 
-    def distill(self) -> None:
-        this_dir = Path(__file__).parent
-        output_dir = (this_dir / "artifacts").resolve()
+    def distill(self, output_dir: Path) -> None:
+        output_dir = output_dir.resolve()
         if output_dir.exists():
             rmtree(output_dir)
         output_dir.mkdir(exist_ok=False)
         for app in self.apps:
             cls = app.__class__
             module_name = cls.__module__.removeprefix("smart_contracts.")
-            qualified_app_name = f"{module_name}.{cls.__name__}"
+            qualified_app_name = f"{module_name}.{cls.__qualname__}"
             app_output_dir = output_dir / qualified_app_name
             app.dump(str(app_output_dir))
 
