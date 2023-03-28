94fc6b179daf9dcb4acf4afd4bcb0f56723e990d
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2023-03-28 15:01:43 +0800

    test: add test to verify default template options

---
+++ tests/test_default_parameters/smart_contracts/helpers/deploy.py
@@ -0,0 +1,33 @@
+import logging
+from collections.abc import Callable
+from pathlib import Path
+
+from algokit_utils.account import Account, get_account
+from algokit_utils.application_specification import ApplicationSpecification
+from algokit_utils.network_clients import get_algod_client, get_indexer_client
+from algosdk.v2client.algod import AlgodClient
+from algosdk.v2client.indexer import IndexerClient
+
+logger = logging.getLogger(__name__)
+
+
+def deploy(
+    app_spec_path: Path,
+    deploy_callback: Callable[
+        [AlgodClient, IndexerClient, ApplicationSpecification, Account], None
+    ],
+    deployer_initial_funds: int = 10000,
+) -> None:
+    # get clients
+    # by default client configuration is loaded from environment variables
+    algod_client = get_algod_client()
+    indexer_client = get_indexer_client()
+
+    # get app spec
+    app_spec = ApplicationSpecification.from_json(app_spec_path.read_text())
+
+    # get deployer account by name, and fund if necessary
+    deployer = get_account(algod_client, "DEPLOYER", fund_with=deployer_initial_funds)
+
+    # use provided callback to deploy the app
+    deploy_callback(algod_client, indexer_client, app_spec, deployer)
