419e2f4d7e191303e8ac839add7e7cd153a5fd85
Altynbek Orumbayev <altynbek.orumbayev@makerx.com.au>
2023-10-17 09:26:54 +0200

    chore: test

---
+++ tests_generated/test_ide_jetbrains-True/smart_contracts/hello_world/contract.py
@@ -0,0 +1,10 @@
+import beaker
+import pyteal as pt
+
+
+app = beaker.Application("hello_world")
+
+
+@app.external
+def hello(name: pt.abi.String, *, output: pt.abi.String) -> pt.Expr:
+    return output.set(pt.Concat(pt.Bytes("Hello, "), name.get()))
