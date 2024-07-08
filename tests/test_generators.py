cba668e2aaecebe25d93a54b2cf0f51d2bf3648f
Altynbek Orumbayev <altynbek.orumbayev@makerx.com.au>
2024-07-08 16:59:16 +0200

    feat: removing .env files and propagating conventions from python template

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -116,7 +116,7 @@ def check_codebase(working_dir: Path, test_name: str) -> subprocess.CompletedPro
     copy_to = working_dir / generated_folder / test_name
 
     # if successful, normalize .copier-answers.yml to make observing diffs easier
-    copier_answers = Path(copy_to / ".copier-answers.yml")
+    copier_answers = Path(copy_to / ".algokit" / ".copier-answers.yml")
     content = copier_answers.read_text("utf-8")
     content = commit_pattern.sub("_commit: <commit>", content)
     content = src_path_pattern.sub("_src_path: <src>", content)
